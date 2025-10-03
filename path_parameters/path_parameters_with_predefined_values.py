dae97efa1636606deac752141e1d5933b5213876
unknown <danielzadva@gmail.com>
2025-10-03 23:06:58 +0100

    path parameter with some predefined values

---
+++ path_parameters/path_parameters_with_predefined_values.py
@@ -0,0 +1,20 @@
+"""Path Parameters with predefined values using Enum"""
+from enum import Enum
+from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    """Predefined values as model"""
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    """Get Model Name"""
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
